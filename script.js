dc72f89694089bfe099904c976d83aaaea201d9c
Jen <evunti@gmail.com>
2024-11-13 10:22:23 -0500

    fixed buttons in css

---
+++ script.js
@@ -0,0 +1,19 @@
+calcButtons = document.getElementById("calculator-keys");
+
+// num = document.getElementsByClassName("num-key");
+// action = document.getElementsByClassName("action-key");
+// modify = document.getElementsByClassName("modify-key");
+
+// calcButtons.addEventListener("click", printNum, false);
+
+const diplay = document.getElementById("display");
+let currentInput = "";
+
+function updateDisplay(num) {
+  currentInput += num;
+  display.value = currentInput;
+}
+function clearDisplay() {
+  currentInput = "";
+  display.value = "";
+}
